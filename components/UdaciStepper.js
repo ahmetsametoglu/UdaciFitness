d951c8f5a3af3b03c5e7ba3fc60988569f1bc011
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-09 21:58:21 +0200

    feat: UdaciStepper component redesigned

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 
-const UdaciStepper = props => {
-  console.log(props.displayName);
+const UdaciStepper = ({ max, unit, step, value, onIncrement, onDecrement }) => {
   return (
     <View>
-      <Text>{props.displayName}</Text>
+      <Text>UdaciSteppers</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name="minus" size={30} color={'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name="plus" size={30} color={'black'} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };
