6e1b854f72d7755c8b381c6f48eb0ce12ea34822
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-09 21:58:55 +0200

    feat: UdaciSlider component redesigned

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Slider } from 'react-native';
 import PropTypes from 'prop-types';
 
-const UdaciSlider = props => {
-  console.log(props.displayName);
-
+const UdaciSlider = ({ max, unit, step, value, onChange }) => {
   return (
     <View>
-      <Text>{props.displayName}</Text>
+      <Text>UdaciSlider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };
