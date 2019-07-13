62cbf0752299bef00ccdf28ef25e655b6cc04866
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-13 11:05:18 +0200

    feat: submit and reset functions implemented to AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import { submitEntry, removeEntry } from '../utils/api';
 
 import UdaciSlider from './UdaciSlider';
 import UdaciStepper from './UdaciStepper';
@@ -65,7 +66,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry });
 
     // Clear local notification
   };
@@ -77,7 +78,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key);
   };
 
   render() {
