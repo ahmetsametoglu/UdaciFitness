cbad54aeead6fc26579a8d17969d0130e305509c
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-25 10:23:32 +0200

    feat: StatusBar implemented

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar, SafeAreaView } from 'react-native';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { purple } from './utils/colors';
+
 import reducer from './store/reducers';
 import TabNav from './components/TabNav';
+import { Constants } from 'expo';
+
+const UdaciStatusBar = ({ backgroundColor, ...props }) => {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
         <TabNav />
-      </View>
+      </SafeAreaView>
     </Provider>
   );
 }
